1de153bd645209ecdae28f0c5b48ed85cb014842
guillermovera <gvera@macalester.edu>
2013-09-10 11:08:09 -0500

    Game of life. Push 1. Guillermo Vera

--- src/edu/macalester/comp124/life/HighLife.java
+++ src/edu/macalester/comp124/life/HighLife.java
@@ -5,10 +5,10 @@ package edu.macalester.comp124.life;
  * @author Michael Ekstrand <ekstrand@cs.umn.edu>
  *
  */
-public class Conway implements RuleSet {
+public class HighLife implements RuleSet {
     
     public String getName() {
-        return "Conway's Rules";
+        return "HighLife";
     }
 
     /**
@@ -25,7 +25,8 @@ public class Conway implements RuleSet {
             return false;
         } else if (!isAlive && neighborCount == 3){
             return true;
-        }
-        return isAlive;
+        } else if (!isAlive && neighborCount == 6){
+            return true;
+        } return isAlive;
     }
 }
